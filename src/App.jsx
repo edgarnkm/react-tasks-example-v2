c7566af2a40821fefdfdb69684a6d73217b32398
edgarnkm <ekasparm@gmail.com>
2024-03-13 16:25:23 -0400

    Mi primer commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Tasklist from './components/TaskList'
+import TaskForm from './components/TaskForm'
+
+function App() {
+
+  return (
+    <main className='bg-zinc-900 h-screen'>
+     <div className='container mx-auto p-10'> 
+      <TaskForm/>
+      <Tasklist />
+      </div>
+    </main>
+  )
+}
+
+export default App
